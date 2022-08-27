a94ddb6f7e71f16ea304125bcb5c9c085af6f8a4
zehao <zehousewood@gmail.com>
2022-08-27 18:10:45 +0800

    create file

--- tools/exportAbc.py
+++ tools/exportAbc.py
@@ -1,2 +1,9 @@
 import sys, os
 from utils import osUtils
+def main(asssetDir):
+    scenes = osUtils.getFilesOfTpye(asssetDir, 'mb')
+    for scene in scenes:
+        print(scene)
+dir1= r'G:\projects\XBL3\pytool\dir1'
+if __name__ == '__main__':
+    main(dir1)
